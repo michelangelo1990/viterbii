05cbf8bd83fe04ca1987bb61bb8959c073a18e1c
michelangelo1990 <mihaela.stojkovic@gmail.com>
2014-10-22 00:01:54 +0200

    konacna

--- vitnovi.py
+++ vitnovi.py
@@ -51,9 +51,10 @@ f.close()
 
 ak=['A','R','N','D','C','Q','E','G','H','I','L','K','M','F','P','S','T','W','Y','V']
 
+
 def mylog(x):
     if x==0:
-        return -100000000
+        return -100000000000000
     else:
         return math.log(x)
 put=dict()
@@ -64,42 +65,49 @@ def viterbi(a):
         s=str.rstrip(a[k])
         x.append(s)
 
-    VM=[[mylog(0) for i in range(dmod+2)] for j in range(L)]
-    VI=[[mylog(0) for i in range(dmod+2)] for j in range(L)]
-    VD=[[mylog(0) for i in range(dmod+2)] for j in range(L)]
+    VM=[[mylog(0) for i in range(dmod+2)] for j in range(L+1)]
+    VI=[[mylog(0) for i in range(dmod+1)] for j in range(L+1)]
+    VD=[[mylog(0) for i in range(dmod+1)] for j in range(L+1)]
     VM[0][0]=0
     
-    for i in range(1,L):
-        razina=0
-        for st in range((dmod)*3):
+    
+    for st in range(1,(dmod+1)*3+1):
+        for i in range(1,L+1):
             j=(st)%3
-            if j==0:
+            razina=st/3
+            if razina==dmod+1:
+                m=max(zip([VM[i][razina-1]+mylog(T[razina-1][0][0]),
+                           VI[i][razina-1]+mylog(T[razina-1][1][0]),
+                           VD[i][razina-1]+mylog(T[razina-1][2][0])],[0,1,2]))
+                VM[i][razina]=m[0]
+                put[(st,i)]=(razina-1,m[1])
+
+            elif j==0:   
                 m=max(zip([VM[i-1][razina-1]+mylog(T[razina][0][0]),
                            VI[i-1][razina-1]+mylog(T[razina][1][0]),
                            VD[i-1][razina-1]+mylog(T[razina][2][0])],[0,1,2]))
-                VM[i][razina]=m[0]+mylog(Me[razina][ak.index(x[i])])
+                VM[i][razina]=m[0]+mylog(Me[razina][ak.index(x[i-1])])
                 put[(st,i)]=(razina-1,m[1])
-                
+                print "stanje %d,%d= %f" %(st,i,VM[i][razina])
             elif j==1:
                 m=max(zip([VM[i-1][razina]+mylog(T[razina][0][1]),
                            VI[i-1][razina]+mylog(T[razina][1][1]),
                            VD[i-1][razina]+mylog(T[razina][2][1])],[0,1,2]))
-                VI[i][razina]=m[0]+mylog(Ie[razina][ak.index(x[i])])
+                VI[i][razina]=m[0]+mylog(Ie[razina][ak.index(x[i-1])])
                 put[(st,i)]=(razina,m[1])
-                razina+=1;
+                print "stanje %d,%d= %f" %(st,i,VI[i][razina]) 
             elif j==2:
                 m=max(zip([VM[i][razina-1]+mylog(T[razina][0][2]),
                       VI[i][razina-1]+mylog(T[razina][1][2]),
                       VD[i][razina-1]+mylog(T[razina][2][2])],[0,1,2]))
                 VD[i][razina]=m[0]
                 put[(st,i)]=(razina-1,m[1])
-                
-    p=math.exp(max(VM[L-1][:]))
-    return put,p
+                print "stanje %d,%d= %f" %(st,i,VD[i][razina])
+    p=math.exp(max(VM[L][:]))
+    
+    return VM,p
 
 vv=viterbi(['H','P','E','W'])
-
-
 
 
 
